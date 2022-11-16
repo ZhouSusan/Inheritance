0ace6c56628cd440360d806935982352912d5f25
ZhouSusan <szhou089@gmail.com>
2022-11-15 16:44:45 -0800

    added Bird class with attributes and methods in Bird.java

--- src/main/java/animals/Bird.java
+++ src/main/java/animals/Bird.java
@@ -1,6 +1,29 @@
 package animals;
 
-public class Bird {
+public class Bird extends Animal {
 
+    private boolean canFly;
 
+    public boolean getCanFly() {
+        return canFly;
+    }
+
+    public Bird(String name,  String type, String color, int height, int weight) {
+        super(name, type, color, height, weight);
+        this.canFly = true;
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Chirk! Chirck!");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("I like to sing and fly.");
+    }
+
+    public void fact() {
+        System.out.println("Ravens are great at mimicking human speech and sounds.");
+    }
 }

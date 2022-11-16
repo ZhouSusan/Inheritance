eabab899d3c8aa5d6bdc1f4da36eeaf287fb2ef8
ZhouSusan <szhou089@gmail.com>
2022-11-15 16:30:15 -0800

    added Animal constrcutor in Animal.java and refactored org.example package name to animals

---
+++ src/main/java/animals/Animal.java
@@ -0,0 +1,16 @@
+package animals;
+
+public class Animal {
+    private String name;
+    private String type;
+    private String color;
+    private int weight, height;
+
+    public Animal(String name, String type, String color, int height, int weight) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
